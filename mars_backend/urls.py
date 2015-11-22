4e6ad883fcb96ab3354ed4b3a211d82d7213c0a7
taotianran <taotianran@limijiaoyin.com>
2015-11-22 23:47:30 +0800

    modify

--- mars_backend/urls.py
+++ mars_backend/urls.py
@@ -6,10 +6,5 @@ import portal.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/article$', portal_views.getArticle),
-    url(r'^api/overview$', portal_views.getOverview),
-    url(r'^api/rank$', portal_views.getRank),
-    url(r'^api/partners$', portal_views.getPartners),
-    url(r'^api/cover$', portal_views.getCover),
-    url(r'^api/share$', portal_views.getShare),
+    url(r'^api/config$', portal_views.getConfig),
 ]
--- portal/views.py
+++ portal/views.py
@@ -6,44 +6,19 @@ import json
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def getArticle(request):
-    articlePath = os.path.join(BASE_DIR, 'json/article.json')
-    article = json.load(open(articlePath))
-    return JsonResponse({
-        'data': article    
-    })
-
-def getOverview(request):
-    overviewPath = os.path.join(BASE_DIR, 'json/overview.json')
-    overview = json.load(open(overviewPath))
-    return JsonResponse({
-        'data': overview    
-    })
-
-def getRank(request):
-    rankPath = os.path.join(BASE_DIR, 'json/rank.json')
-    rank = json.load(open(rankPath))
-    return JsonResponse({
-        'data': rank    
-    })
-
-def getPartners(request):
-    partnersPath = os.path.join(BASE_DIR, 'json/partners.json')
-    partners = json.load(open(partnersPath))
-    return JsonResponse({
-        'data': partners    
-    })
-
-def getCover(request):
-    coverPath = os.path.join(BASE_DIR, 'json/cover.json')
-    cover = json.load(open(coverPath))
-    return JsonResponse({
-        'data': cover    
-    })
-
-def getShare(request):
-    sharePath = os.path.join(BASE_DIR, 'json/share.json')
-    share = json.load(open(sharePath))
-    return JsonResponse({
-        'data': share    
-    })
+def getConfig(request):
+    versionName = request.GET.get('id')
+    configPath = os.path.join(BASE_DIR, 'json/config.json')
+    config = json.load(open(configPath))
+    name = config[versionName];
+    if name:
+        fileName = '%s.json' % name
+        filePath = os.path.join(BASE_DIR, 'json/%s' % fileName);
+        data = json.load(open(filePath))
+        return JsonResponse({
+            'data': data
+        })
+    else:
+        return JsonResponse({
+            'data': {}    
+        })
